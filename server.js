767bcc109969278653948849eed296122c481fd3
AvivSarig <avivs15@gmail.com>
2024-01-10 15:29:40 +0100

    save progress - remove middlewares

--- server.js
+++ server.js
@@ -1,39 +1,50 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const fs = require("fs");
+const dotenv = require("dotenv");
 
-process.on('uncaughtException', err => {
-  console.log(`UNCAUGHT EXCEPTION! 💥 Shutting down...`);
-  // console.log(err.name, err.message);
-  process.exit(1);
+process.on("uncaughtException", (err) => {
+	console.log(`UNCAUGHT EXCEPTION! 💥 Shutting down...`);
+	console.log(err.name + ":");
+	console.log(err.message);
+	console.log(err.stack);
+	process.exit(1);
 });
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
-const app = require('./app');
+const app = require("./app");
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+// Look for database and password in env., else go to config file
+let DB;
+if (process.env.DATABASE && process.env.DATABASE_PASSWORD) {
+	DB = process.env.DATABASE.replace(
+		"<PASSWORD>",
+		process.env.DATABASE_PASSWORD
+	);
+} else {
+	const config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
+	DB = config.database.connectionString.replace(
+		"<PASSWORD>",
+		config.database.password
+	);
+}
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('DB connection successful'));
+	.connect(DB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("DB connection successful"));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+	console.log(`App running on port ${port}`);
 });
 
-process.on('unhandledRejection', err => {
-  console.log(`UNHANDLED REJECTION! 💥 Shutting down...`);
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
+process.on("unhandledRejection", (err) => {
+	console.log(`UNHANDLED REJECTION! 💥 Shutting down...`);
+	console.log(err.name, err.message);
+	server.close(() => {
+		process.exit(1);
+	});
 });
