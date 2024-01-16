eef356c850b899b813547dde59c0ef6e47561c8b
AvivSarig <avivs15@gmail.com>
2024-01-16 12:47:20 +0100

    save progress

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -43,6 +43,12 @@ exports.getLoginForm = (req, res) => {
 	});
 };
 
+exports.getSignupForm = (req, res) => {
+	res.status(200).render("signup", {
+		title: "Signup",
+	});
+};
+
 exports.getAccount = (req, res) => {
 	res.status(200).render("account", {
 		title: "My Account",
