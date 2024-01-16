edec027398928a7cac1d5e9df1d79d5b568ed384
AvivSarig <avivs15@gmail.com>
2024-01-16 12:17:57 +0100

    eslint checking added

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,82 +1,82 @@
-const Tour = require('../models/tourModel');
-const User = require('../models/userModel');
-const Booking = require('../models/bookingModel');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const Tour = require("../models/tourModel");
+const User = require("../models/userModel");
+const Booking = require("../models/bookingModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1) Get the tour data from collection
-  const tours = await Tour.find();
+	// 1) Get the tour data from collection
+	const tours = await Tour.find();
 
-  // 2) build template
+	// 2) build template
 
-  // 3) Render the template using the data
+	// 3) Render the template using the data
 
-  res.status(200).render('overview', {
-    title: 'All Tours',
-    tours
-  });
+	res.status(200).render("overview", {
+		title: "All Tours",
+		tours,
+	});
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  // 1) Get the tour data, including reviews and guides
-  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-    path: 'reviews',
-    fields: 'review rating user'
-  });
+	// 1) Get the tour data, including reviews and guides
+	const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+		path: "reviews",
+		fields: "review rating user",
+	});
 
-  if (!tour) {
-    return next(new AppError('Tour not found', 404));
-  }
+	if (!tour) {
+		return next(new AppError("Tour not found", 404));
+	}
 
-  // 2) build template
-  // 3) Render the template using the data
-  res.status(200).render('tour', {
-    title: `${tour.name} Tour`,
-    tour
-  });
+	// 2) build template
+	// 3) Render the template using the data
+	res.status(200).render("tour", {
+		title: `${tour.name} Tour`,
+		tour,
+	});
 });
 
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', {
-    title: 'Login'
-  });
+	res.status(200).render("login", {
+		title: "Login",
+	});
 };
 
 exports.getAccount = (req, res) => {
-  res.status(200).render('account', {
-    title: 'My Account'
-  });
+	res.status(200).render("account", {
+		title: "My Account",
+	});
 };
 
 exports.getMyTours = async (req, res, next) => {
-  // 1) find all bookings
-  const bookings = await Booking.find({ user: req.user.id });
+	// 1) find all bookings
+	const bookings = await Booking.find({ user: req.user.id });
 
-  // 2) find tours with the returned id's
-  const tourIDs = bookings.map(el => el.tour);
-  const tours = await Tour.find({ _id: { $in: tourIDs } });
+	// 2) find tours with the returned id's
+	const tourIDs = bookings.map((el) => el.tour);
+	const tours = await Tour.find({ _id: { $in: tourIDs } });
 
-  res.status(200).render('overview', {
-    title: 'My Tours',
-    tours
-  });
+	res.status(200).render("overview", {
+		title: "My Tours",
+		tours,
+	});
 };
 
 exports.updateUserData = catchAsync(async (req, res, next) => {
-  const updatedUser = await User.findByIdAndUpdate(
-    req.user.id,
-    {
-      name: req.body.name,
-      email: req.body.email
-    },
-    {
-      new: true,
-      runValidators: true
-    }
-  );
-  res.status(200).render('account', {
-    title: 'My Account',
-    user: updatedUser
-  });
+	const updatedUser = await User.findByIdAndUpdate(
+		req.user.id,
+		{
+			name: req.body.name,
+			email: req.body.email,
+		},
+		{
+			new: true,
+			runValidators: true,
+		}
+	);
+	res.status(200).render("account", {
+		title: "My Account",
+		user: updatedUser,
+	});
 });
